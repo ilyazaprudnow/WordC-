dea453eb782bd70715998b4d7b460d53fe588f52
Илья Запруднов <IlyaGo.IlyaGo@outlook.com>
2020-05-25 16:30:39 +0300

    commit1

--- WordCSharp/Program.cs
+++ WordCSharp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
 
 namespace WordCSharp
 {
@@ -10,6 +11,51 @@ namespace WordCSharp
     {
         static void Main(string[] args)
         {
+            Application app = new Application();
+            Document test = app.Documents.Add(Visible: true);
+            Range r = test.Range();
+            r.Text = "Hello, Word";
+            //r.Bold = 20;
+
+            Table tab = test.Tables.Add(r, 5, 5);
+            tab.Borders.Enable = 1;
+
+            foreach (Row row in tab.Rows)
+            {
+                foreach (Cell cell in row.Cells)
+                {
+                    if (cell.RowIndex == 1)
+                    {
+                        cell.Range.Text = "Колонка " + cell.ColumnIndex.ToString();
+                        cell.Range.Bold = 1;
+                        cell.Range.Font.Name = "Arial";
+                        cell.Range.Font.Size = 14;
+
+                        cell.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+                        cell.Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                    }
+                    else
+                    {
+                        //cell.Range.Text = (cell.RowIndex - 2 + cell.ColumnIndex).ToString();
+                        cell.Range.Text = "Hello, Word";
+                    }
+                }
+            }
+
+            test.Save();
+            app.Documents.Open("C:\\Users\\Илья\\Desktop\\Doc2.docx");
+            Console.ReadKey();
+            try
+            {
+                test.Close();
+                app.Quit();
+            }
+            catch (Exception a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            Console.ReadKey();
+
         }
     }
 }
